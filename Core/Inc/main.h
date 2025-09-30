e26432e8b303d1b4ce5d75971ad55034c0ee25fd
Wey Théo <wey.theo@outlook.fr>
2025-09-30 15:54:52 +0200

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define T_NRST_Pin GPIO_PIN_2
+#define T_NRST_GPIO_Port GPIOF
+#define CAPT_HUM_Pin GPIO_PIN_0
+#define CAPT_HUM_GPIO_Port GPIOA
+#define CAPT_TEMP_Pin GPIO_PIN_1
+#define CAPT_TEMP_GPIO_Port GPIOA
+#define T_VCP_TX_Pin GPIO_PIN_2
+#define T_VCP_TX_GPIO_Port GPIOA
+#define T_VCP_RX_Pin GPIO_PIN_3
+#define T_VCP_RX_GPIO_Port GPIOA
+#define FAN_Pin GPIO_PIN_2
+#define FAN_GPIO_Port GPIOB
+#define PUMP_Pin GPIO_PIN_8
+#define PUMP_GPIO_Port GPIOA
+#define LD3_Pin GPIO_PIN_6
+#define LD3_GPIO_Port GPIOC
+#define T_JTMS_Pin GPIO_PIN_13
+#define T_JTMS_GPIO_Port GPIOA
+#define T_JTCK_Pin GPIO_PIN_14
+#define T_JTCK_GPIO_Port GPIOA
+#define PSH_BUT_DOWN_Pin GPIO_PIN_6
+#define PSH_BUT_DOWN_GPIO_Port GPIOB
+#define PSH_BUT_UP_Pin GPIO_PIN_7
+#define PSH_BUT_UP_GPIO_Port GPIOB
+#define PSH_BUT_SEL_Pin GPIO_PIN_8
+#define PSH_BUT_SEL_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
