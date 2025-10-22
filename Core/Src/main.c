d633d0f17ac06cde40310b0d230a49ae444494a9
TheoWey <wey.theo@outlook.fr>
2025-10-22 08:59:30 +0200

    [DEV] main_serre: integrate PWM handling for pump and fan control

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -102,7 +102,7 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+    main_serre();
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
--- Core/serre/main_serre.cc
+++ Core/serre/main_serre.cc
@@ -4,6 +4,8 @@
 #include "driver/sensors/soil_hum_sensor/inc/soil_hum.hh"
 #include "driver/sensors/temp_sensor/inc/temp_sensor.hh"
 
+#include "driver/PWM/inc/pwm.hh"
+
 void main_serre(void) {
 
     static sensor::SensorConfig tempConfig = {&hadc1, ADC_CHANNEL_1,
@@ -12,8 +14,14 @@ void main_serre(void) {
     static sensor::SensorConfig soilHumConfig = {
         &hadc1, ADC_CHANNEL_0, ADC_SAMPLINGTIME_COMMON_1, 100};
 
+    static pwm::pwm_handler_t pwm_pumpHandler = {&htim1, TIM_CHANNEL_1};
+    static pwm::pwm_handler_t pwm_fanHandler = {&htim1, TIM_CHANNEL_2};
+
     static sensor::SoilHumSensor soilHumSensor(soilHumConfig);
     static sensor::TempSensor tempSensor(tempConfig);
+
+    static pwm::PWM pumpPWM(pwm_pumpHandler);
+    static pwm::PWM fanPWM(pwm_fanHandler);
 
     float temperature = 0.0f;
     float humidity = 0.0f;
@@ -27,5 +35,21 @@ void main_serre(void) {
         soilHumSensor.processData();
         humidity = soilHumSensor.getHumidityPercent();
     }
+
+    pumpPWM.setDutyCycle(humidity / 100.0f);
+    fanPWM.setDutyCycle(temperature / 100.0f);
+
+    if (temperature < 20.0f) {
+        fanPWM.disable();
+    } else {
+        fanPWM.enable();
+    }
+
+    if (humidity < 40.0f) {
+        pumpPWM.enable();
+    } else {
+        pumpPWM.disable();
+    }
+
     HAL_Delay(1000);
 }
