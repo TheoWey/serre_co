640f51e4f66db1eac899d3e880ee5103f99901b1
TheoWey <wey.theo33@gmail.com>
2025-11-01 18:57:59 +0100

    [DEV] sensor: Implement CircularBuffer class and integrate it into Sensor for ADC data handling

--- Core/serre/main_serre.cc
+++ Core/serre/main_serre.cc
@@ -12,8 +12,8 @@ void main_serre(void) {
         adc_manager::ADCManager::getInstance();
 
     if (adcManager.getConversionCompleteFlag()) {
-        sensorManager.readAllSensors();
-        sensorManager.processAllSensors();
+        auto &sensorManager = sensor::SensorManager::getInstance();
+        sensorManager.updateAllSensors();
         adcManager.setConversionCompleteFlag(false);
     }
 }
@@ -24,12 +24,10 @@ void main_serre_init(void) {
     static adc_manager::ADCManager &adcManager =
         adc_manager::ADCManager::getInstance();
 
-    static sensor::TempSensor tempSensor0(&adcManager, 1);
-    static sensor::SoilHumSensor soilHumSensor0(&adcManager, 0);
+    static sensor::TempSensor tempSensor0(&adcManager, 1);       // Channel 1
+    static sensor::SoilHumSensor soilHumSensor0(&adcManager, 0); // Channel 0
 
-    static sensor::SensorManager &sensorManager =
-        sensor::SensorManager::getInstance();
-
+    sensor::SensorManager &sensorManager = sensor::SensorManager::getInstance();
     sensorManager.subscribeTempSensor(&tempSensor0);
     sensorManager.subscribeSoilHumSensor(&soilHumSensor0);
 
--- Core/serre/main_serre.h
+++ Core/serre/main_serre.h
@@ -7,6 +7,7 @@ extern "C" {
 
 // Function prototypes
 void main_serre(void);
+void main_serre_init(void);
 
 #ifdef __cplusplus
 }
