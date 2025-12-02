f4e9ff17527af474030a2e49cf4709ce7da6bc8e
Wey Théo <wey.theo@outlook.fr>
2025-12-02 12:08:23 +0100

    Revert "[MINOR] main_serre: refactor sensor handling to improve temperature and humidity updates"
    This reverts commit c3dba44804e26967ab18485a392e5cbfa898224c.

--- Core/serre/main_serre.cc
+++ Core/serre/main_serre.cc
@@ -18,39 +18,33 @@ void main_serre(void) {
 
         static float lastTemp = -1000.0f;
         static float lastSoilHum = -1.0f;
-        float temperature = -1000.0f;
-        float soilHumidity = -1.0f;
 
         if (sensorManager.getTempSensor(0) != nullptr) {
-            temperature =
+            float temperature =
                 sensorManager.getTempSensor(0)->getTemperatureCelsius();
+            lastTemp = temperature;
             float dutyCycle =
                 ((temperature * 100.0f) / 50.0f) / 100; // Scale 20-40C
             pwm::PWMManager::getInstance().setDutyCycle(
                 0, dutyCycle); // Control first PWM channel
         }
         if (sensorManager.getSoilHumSensor(0) != nullptr) {
-            soilHumidity =
+            float soilHumidity =
                 sensorManager.getSoilHumSensor(0)->getHumidityPercent();
+            lastSoilHum = soilHumidity;
             float dutyCycle = (100.0f - soilHumidity) / 100.0f; // Scale 0-100%
             pwm::PWMManager::getInstance().setDutyCycle(
                 1, dutyCycle); // Control second PWM channel
         }
-        if (temperature != lastTemp + 0.5 || temperature != lastTemp - 0.5 ||
-            soilHumidity != lastSoilHum + 1.0f ||
-            soilHumidity != lastSoilHum - 1.0f) {
-            lastSoilHum = soilHumidity;
-            lastTemp = temperature;
-            print_status(lastTemp, lastSoilHum);
-        }
+        print_status(lastTemp, lastSoilHum);
     }
 
     pwm::PWMManager::getInstance().enableAll(true);
 }
 
 void main_serre_init(void) {
-    i2c::init_i2c();
     lcd::init_lcd();
+    i2c::init_i2c();
     pwm::init_pwm();
     adc_manager::init_adc();
     sensor::init_sensors();
