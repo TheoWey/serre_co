70a9e0a540c69edfcbadc8f9e3d9846477109032
TheoWey <wey.theo33@gmail.com>
2025-12-08 19:24:39 +0100

    [DEV] add temperature and humidity setpoint management methods to SerreController

--- Core/serre/UTILS/mae_serre/inc/mae_serre.hh
+++ Core/serre/UTILS/mae_serre/inc/mae_serre.hh
@@ -14,6 +14,10 @@ class SerreController {
 
     void processLoop();
 
+    void pwm_control_loop(void);
+
+    void sensor_update_loop(void);
+
     inline SerreMode getCurrentMode() const {
         return currentMode_;
     }
@@ -21,6 +25,38 @@ class SerreController {
     inline void setMode(SerreMode newMode) {
         currentMode_ = newMode;
     };
+
+    inline void SetTemperatureSetpoint(uint16_t setpoint) {
+        temperatureSetpoint_ = setpoint;
+    }
+
+    inline void SetTemperatureResetPoint(uint16_t resetPoint) {
+        temperatureResetPoint_ = resetPoint;
+    }
+
+    inline void SetHumiditySetpoint(uint16_t setpoint) {
+        humiditySetpoint_ = setpoint;
+    }
+
+    inline void SetHumidityResetPoint(uint16_t resetPoint) {
+        humidityResetPoint_ = resetPoint;
+    }
+
+    inline uint16_t GetTemperatureSetpoint() const {
+        return temperatureSetpoint_;
+    }
+
+    inline uint16_t GetTemperatureResetPoint() const {
+        return temperatureResetPoint_;
+    }
+
+    inline uint16_t GetHumiditySetpoint() const {
+        return humiditySetpoint_;
+    }
+
+    inline uint16_t GetHumidityResetPoint() const {
+        return humidityResetPoint_;
+    }
 
   private:
     SerreController() : currentMode_(SerreMode::NORMAL) {
@@ -35,6 +71,11 @@ class SerreController {
     uint32_t pwmUpdatePeriodMs_ = 3000;
     uint32_t lastSettingModeTick_ = 0;
     uint32_t settingModeTimeoutMs_ = 60000;
+
+    uint16_t temperatureSetpoint_ = 25;
+    uint16_t temperatureResetPoint_ = 20;
+    uint16_t humiditySetpoint_ = 50;
+    uint16_t humidityResetPoint_ = 40;
 };
 
 void pwm_control_loop(void);
